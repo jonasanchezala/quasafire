6c4f578f2dfde6b65a9699b2f33ec9e05834ceb4
jonasanchezala <jonasanchezala@unal.edu.co>
2021-07-09 09:26:42 -0500

    Add init project files

---
+++ src/main/java/com/meli/challenge/quasarfire/QuasarfireApplication.java
@@ -0,0 +1,13 @@
+package com.meli.challenge.quasarfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarfireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/challenge/quasarfire/QuasarfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.challenge.quasarfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
