030b41f0768160583dd55aaf068addebf8fe4071
jonasanchezala <jonasanchezala@unal.edu.co>
2021-07-16 17:02:34 -0500

    Create services to split top secret

--- src/main/java/com/meli/challenge/quasarfire/model/SatelliteRequest.java
+++ src/main/java/com/meli/challenge/quasarfire/model/SatelliteRequest.java
@@ -1,9 +1,11 @@
 package com.meli.challenge.quasarfire.model;
 
 import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
 public class SatelliteRequest {
--- src/main/java/com/meli/challenge/quasarfire/model/TopSecretRequest.java
+++ src/main/java/com/meli/challenge/quasarfire/model/TopSecretRequest.java
@@ -1,10 +1,11 @@
 package com.meli.challenge.quasarfire.model;
 
+import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
+@Builder
 @Getter
 @Setter
 public class TopSecretRequest {
